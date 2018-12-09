49fbf4a33853e1c0184d4ac0fbf0d9a71172bd48
ma <marisancans@gmail.com>
2018-12-09 18:22:22 +0200

    Speed, brightness added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 //Sensor readings with offsets:   1       -2      16380   0       0       -2
 //Your offsets:   -3931   2067    989     74      108     21
 
+// 191     -355    1497    -144    155     12 hat with plastic  ball
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 
+#include <algorithm>
 #include <Wire.h>
 #include <FastLED.h>
 #include "MPU6050.h"
@@ -12,6 +14,8 @@
 #define NUM_LEDS 30
 #define PIN D4
 #define COLOR_ORDER RGB
+uint8_t globalBrightness = 5;
+int speed = 100;
 
 
 // MPU settings 
@@ -39,18 +43,26 @@ void setupMPU(){
     Wire.begin(sdaMPU, sclMPU);
     Serial.println("Initialize MPU");
     mpu.initialize();
-    mpu.setXAccelOffset(-3931);
-    mpu.setYAccelOffset(2067);
-    mpu.setZAccelOffset(989);
-    mpu.setXGyroOffset(74);
-    mpu.setYGyroOffset(108);
-    mpu.setZGyroOffset(21);
+    mpu.setXAccelOffset(191);
+    mpu.setYAccelOffset(-355);
+    mpu.setZAccelOffset(1497);
+    mpu.setXGyroOffset(-144);
+    mpu.setYGyroOffset(155);
+    mpu.setZGyroOffset(12);
+    // mpu.setXAccelOffset(-3931);
+    // mpu.setYAccelOffset(2067);
+    // mpu.setZAccelOffset(989);
+    // mpu.setXGyroOffset(74);
+    // mpu.setYGyroOffset(108);
+    // mpu.setZGyroOffset(21);
     mpuStatus = mpu.testConnection();
     Serial.println(mpuStatus ? "Connected" : "Connection failed");
+    
 }
 
 void setup() { 
   Serial.begin(115200);
+  while (! Serial);
   FastLED.addLeds<WS2812B, PIN>(leds, NUM_LEDS); FastLED.setBrightness( 5 );
   setupMPU();
   pinMode(D0, OUTPUT);
@@ -88,8 +100,8 @@ void loop() {
   static uint8_t hue;
   auto thresh = 2;
 
-  if(normX || normY || normZ){
-    leds[NUM_LEDS/2] = CRGB(normY, normX, normZ);
+  if(normX > 4 || normY > 4 || normZ > 4){
+    leds[NUM_LEDS/2] = CHSV(hue++, 255, 255);//CRGB(normY, normX, normZ);
   } else {
     leds[NUM_LEDS/2] = CRGB(0, 0, 0);
   }
@@ -105,10 +117,70 @@ void loop() {
 
 
   FastLED.show();
-  delay(100);
+  delay(speed);
  
- //printMotion();
-}
+ printMotion();
 
+
+ if (Serial.available())
+  {
+    char ch = Serial.read();
+    if (ch >= '0' && ch <= '9')
+    {
+      
+      int x = ch - '0';
+      Serial.print("Value red: ");
+      Serial.println(x);
+
+      switch(x) {
+          case 0 : FastLED.setBrightness(0);break; 
+          case 1 : FastLED.setBrightness(1);  break;
+          case 2 : FastLED.setBrightness(2); ; break;
+          case 3 : FastLED.setBrightness(3); break;
+          case 4 : FastLED.setBrightness(4); break;
+          case 5 : FastLED.setBrightness(5); ;break;
+          case 6 : FastLED.setBrightness(10); break;
+          case 7 : FastLED.setBrightness(20); break;
+          case 8 : FastLED.setBrightness(127); break;
+          case 9 : FastLED.setBrightness(255); 
+      }
+
+
+    }
+
+    if (ch == '+')
+      FastLED.setBrightness(globalBrightness++);
+
+    if (ch == '-')
+      FastLED.setBrightness(globalBrightness--);
+
+    if (ch == '.')
+      speed += 10;
+
+    if (ch == ',')
+      speed -= 10;
+
+
+    if (ch == 'a')
+      FastLED.setMaxPowerInVoltsAndMilliamps(3.3, 500);
+
+    if (ch == 'b')
+      FastLED.setMaxPowerInVoltsAndMilliamps(1.5, 800);
+
+    if (ch == 'c')
+      FastLED.setMaxPowerInVoltsAndMilliamps(1, 1000);
+    
+
+    Serial.print("Brightness set to: ");
+      Serial.println(FastLED.getBrightness());
+    Serial.print("Speed set to: ");
+      Serial.println(speed);
+
+  }
+
+
+
+
+}
 
 
